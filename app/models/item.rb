626fd7b6134e2ab9b4db7990f433f6764f69c9bb
kopelwende <kopelwende@gmail.com>
2022-11-21 11:11:23 +0900

    ActiveHash モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,21 @@
 class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :item_status_id, presence: true
-  validates :shipping_charges_id, presence: true
-  validates :prefectures_id, presence: true
-  validates :estimated_shipping_id, presence: true
+  validates :category_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_status_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :shipping_charges_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefectures_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
+  validates :estimated_shipping_id, presence: true, numericality: {other_than: 1, message: "can't be blank"}
   validates :price, presence: true
   validates :user, presence: true
 
   belongs_to :user
   has_one_attached :image
+
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_charges
+  belongs_to :prefectures
+  belongs_to :estimated_shipping
+
 end
