22692f943c23291e8fd546c590a1a17b02abfece
kopelwende <kopelwende@gmail.com>
2022-11-21 12:55:05 +0900

    Itemsコントローラー createアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item =Item.new(item_params)
+    if @item.save
+      redirecrt_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def item_params
+    params.requier(:item).permit(:item_name,:explanation,:category_id,:item_status_id,:shipping_charges_id,:prefectures_id,:estimated_shipping_id,:image)
 end
