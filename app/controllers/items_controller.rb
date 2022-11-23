3406d7ace4b323c31d00e4607f32b3c38074de83
kopelwende <kopelwende@gmail.com>
2022-11-23 11:44:48 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :go_to_index,only: :new
+  before_action :go_to_index, only: :new
 
   def index
-  
   end
 
   def new
@@ -18,18 +17,16 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
   private
 
   def item_params
-    params.require(:item).permit(:item_name,:explanation,:category_id,:item_status_id,:shipping_charges_id,:prefectures_id,:estimated_shipping_id,:price,:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :explanation, :category_id, :item_status_id, :shipping_charges_id, :prefectures_id,
+                                 :estimated_shipping_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def go_to_index 
-    unless user_signed_in? 
-      redirect_to new_user_session_path
-    end
-  end
+  def go_to_index
+    return if user_signed_in?
 
+    redirect_to new_user_session_path
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,22 +3,22 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :explanation, presence: true
-  validates :category_id,  numericality: {other_than: 1, message: "can't be blank"}
-  validates :item_status_id, numericality: {other_than: 1, message: "can't be blank"}
-  validates :shipping_charges_id,numericality: {other_than: 1, message: "can't be blank"}
-  validates :prefectures_id, numericality: {other_than: 1, message: "can't be blank"}
-  validates :estimated_shipping_id,  numericality: {other_than: 1, message: "can't be blank"}
-  validates :price, presence: true,format:{ with: /\A[0-9]+\z/ }, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to:9999999, message:"is out of setting range"}
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_charges_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :estimated_shipping_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ },
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
   validates :user, presence: true
 
   belongs_to :user
   has_one_attached :image
-  validates :image,presence:true
+  validates :image, presence: true
 
   belongs_to :category
   belongs_to :item_status
   belongs_to :shipping_charges
   belongs_to :prefectures
   belongs_to :estimated_shipping
-
 end
