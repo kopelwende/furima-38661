68a0c33414bc90d44ed67a83f920f9175ab2d215
kopelwende <kopelwende@gmail.com>
2022-11-28 13:50:23 +0900

    商品購入テストコード準備

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :order_address do
-    association :user
-    association :item
+    user_id{association :user}
+    item_id{association :item}
     
-    post_code {123-4567}
+    post_code {"123-4567"}
     prefectures_id { Faker::Number.between(from: 2, to: 48) }
     municipality { Faker::Address.city }
     address { Faker::Address.zip_code }
-    bilding { Faker::Lorem.characters }
-    phone_number { Faker::Number.between(from: 10, to: 11)}
+    building { Faker::Lorem.sentence }
+    phone_number {12345678900}
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,6 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
+  before do
+    @order_address = FactoryBot.build(:order_address)
+  end
 
+  describe "購入できる時" do
+    it "全ての項目を正常に入力すると購入できる" do
+    expect(@order_address).to be_valid
+    end
+
+    it "birdingが空白でも購入できる" do
+    end
+  end
+
+  describe "購入できない時" do
+    it "userが紐づいていないと登録できない" do
+    end
+
+    it "itemが紐づいていないと登録できない" do
+    end
+
+    it "post_codeが空だと登録できない" do
+    end
+
+    it "post_codeが英字だと登録できない" do
+    end
+
+    it "post_codeが全角だと登録できない" do
+    end
+
+    it"prefectures_idが空だと登録できない" do
+    end
+    
+    it"municipalityが空だと登録できない" do
+    end
+    
+    it"addressが空だと登録できない" do
+    end
+    
+    it"phone_numberが空だと登録できない" do
+    end
+
+    it "phone_numberが英字だと登録できない" do
+    end
+
+    it "phone_numberが全角だと登録できない" do
+    end
+  end
 end
 
