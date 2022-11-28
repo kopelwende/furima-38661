c50e889516da620cb51a07f7647332abf869ee50
kopelwende <kopelwende@gmail.com>
2022-11-28 14:57:02 +0900

    商品購入テストコード実行

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -11,41 +11,76 @@ RSpec.describe OrderAddress, type: :model do
     end
 
     it "birdingが空白でも購入できる" do
+      @order_address.building = ""
+      expect(@order_address).to be_valid
     end
   end
 
   describe "購入できない時" do
-    it "userが紐づいていないと登録できない" do
+    it "userが紐づいていないと購入できない" do
+      @order_address.user_id = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("User can't be blank")
     end
 
-    it "itemが紐づいていないと登録できない" do
+    it "itemが紐づいていないと購入できない" do
+      @order_address.item_id = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Item can't be blank")
     end
 
-    it "post_codeが空だと登録できない" do
+    it "post_codeが空だと購入できない" do
+      @order_address.post_code = ""
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Post code can't be blank", "Post code is invalid")
     end
 
-    it "post_codeが英字だと登録できない" do
+    it "post_codeが英字だと購入できない" do
+      @order_address.post_code = "abcdefg"
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Post code is invalid")
     end
 
-    it "post_codeが全角だと登録できない" do
+    it "post_codeが全角だと購入できない" do
+      @order_address.post_code = "１１１−１１１１"
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Post code is invalid")
     end
 
-    it"prefectures_idが空だと登録できない" do
+    it"prefectures_idが空だと購入できない" do
+      @order_address.prefectures_id = ""
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Prefectures can't be blank")
     end
     
-    it"municipalityが空だと登録できない" do
+    it"municipalityが空だと購入できない" do
+      @order_address.municipality = ""
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Municipality can't be blank")
     end
     
-    it"addressが空だと登録できない" do
+    it"addressが空だと購入できない" do
+      @order_address.address = ""
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Address can't be blank")
     end
     
-    it"phone_numberが空だと登録できない" do
+    it"phone_numberが空だと購入できない" do
+      @order_address.phone_number = ""
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number can't be blank", "Phone number is invalid")
     end
 
-    it "phone_numberが英字だと登録できない" do
+    it "phone_numberが英字だと購入できない" do
+      @order_address.phone_number = "abcdefg"
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number is invalid")
     end
 
-    it "phone_numberが全角だと登録できない" do
+    it "phone_numberが全角だと購入できない" do
+      @order_address.phone_number = "１２３４５６７８９００"
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number is invalid")
     end
   end
 end
